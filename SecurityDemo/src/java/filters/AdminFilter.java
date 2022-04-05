283bec9bfaa9850abc9b4aac447f47be867adc73
CapBarbosssa <97461923+CapBarbosssa@users.noreply.github.com>
2022-04-04 21:39:43 -0600

    final commit

--- SecurityDemo/src/java/filters/AdminFilter.java
+++ SecurityDemo/src/java/filters/AdminFilter.java
@@ -18,6 +18,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.Role;
 import models.User;
 import services.AccountService;
 
@@ -41,31 +42,28 @@ public class AdminFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest httpRequest =(HttpServletRequest) request;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         // check if the user is a admin
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
         HttpSession session = httpRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        
+ 
         
         AccountService as = new AccountService();
-        User user = as.login(email, password);
-        session.setAttribute("email", email);
+        User user = as.login(email, "password");
+          Role role = new Role();
      
-        if (user.getRole().getRoleId() == 1 || user != null) {
+        if (user != null && user.getRole().getRoleId() == 1 ) {
      
-            httpResponse.sendRedirect("admin");
+            chain.doFilter(request, response);
           
         } else {
-            httpResponse.sendRedirect("notes");
+            httpResponse.sendRedirect("login");
             
-        }
-        
-        chain.doFilter(request, response);
- 
-     
-        
-       
+        }       
+      
+
     }
 
 
